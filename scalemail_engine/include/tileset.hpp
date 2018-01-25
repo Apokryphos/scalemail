57e88e5c1a59f79faab76865a9f8779c71f99caa
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-01-25 11:20:16 -0500

    Added Tileset class.

--- scalemail_engine/include/tileset.hpp
+++ scalemail_engine/include/tileset.hpp
@@ -1,9 +1,20 @@
 #pragma once
 
-#include <glm/vec3.hpp>
+#include "texture.hpp"
+#include <glm/vec2.hpp>
 
 namespace ScaleMail
 {
+class Tileset
+{
+public:
+	Tileset(Texture texture, int tileWidth, int tileHeight);
+	void getTileUv(int index, glm::vec2& uv1, glm::vec2& uv2);
+    Texture texture;
+	const int tileHeight;
+	const int tileWidth;
+};
+
 void getTilesetUv(const int index, const int width, const int height,
                   const int tileWidth, const int tileHeight,
                   glm::vec2& uv1, glm::vec2& uv2);
--- scalemail_engine/src/tileset.cpp
+++ scalemail_engine/src/tileset.cpp
@@ -1,7 +1,34 @@
-#include <glm/glm.hpp>
+#include "tileset.hpp"
 
 namespace ScaleMail
 {
+//  ============================================================================
+Tileset::Tileset(Texture texture, int tileWidth, int tileHeight)
+    : tileHeight(tileHeight), tileWidth(tileWidth) {
+    this->texture = texture;
+}
+
+//  ============================================================================
+void Tileset::getTileUv(int index, glm::vec2& uv1, glm::vec2& uv2) {
+    const int TILES_PER_ROW = this->texture.width / this->tileWidth;
+
+    int x = index % TILES_PER_ROW;
+    int y = index / TILES_PER_ROW;
+
+    int pixelX = x * this->tileWidth;
+    int pixelY = y * this->tileHeight;
+
+    double u1 = pixelX / (double)this->texture.width;
+    double v1 = pixelY / (double)this->texture.height;
+
+    double u2 = (pixelX + this->tileWidth) / (double)this->texture.width;
+    double v2 = (pixelY + this->tileHeight) / (double)this->texture.height;
+
+    uv1 = glm::vec2(u1, v1);
+    uv2 = glm::vec2(u2, v2);
+}
+
+
 //  ============================================================================
 void getTilesetUv(const int index, const int width, const int height,
                   const int tileWidth, const int tileHeight,

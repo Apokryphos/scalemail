127d55d0c1ed752a6e7b59068a1ef3e11217229f
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-01-24 22:18:04 -0500

    Added EntitySystem class.

---
+++ scalemail_engine/include/entity_system.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "entity.hpp"
+#include <unordered_map>
+
+namespace ScaleMail
+{
+class EntityManager;
+
+class EntitySystem
+{
+protected:
+	size_t mComponentCount;
+
+	EntityManager& mEntityManager;
+	std::unordered_map<Entity, int> mComponentIndicesByEntity;
+	std::unordered_map<int, Entity> mEntitiesByComponentIndices;
+
+	virtual void createComponent(const Entity& entity) = 0;
+	virtual void destroyComponent(int index) = 0;
+	const Entity& getEntityByComponentIndex(const int index) const;
+
+public:
+	EntitySystem(EntityManager& entityManager, int maxComponents);
+	void addComponent(const Entity& entity);
+	void garbageCollect();
+	EntityManager& getEntityManager();
+	bool hasComponent(const Entity& entity) const;
+	const int maxComponents;
+	void removeComponent(const Entity& entity);
+};
+}
---
+++ scalemail_engine/src/entity_system.cpp
@@ -0,0 +1,99 @@
+#include "entity_system.hpp"
+#include "entity_manager.hpp"
+#include <cassert>
+
+namespace ScaleMail
+{
+//	============================================================================
+EntitySystem::EntitySystem(EntityManager& entityManager, int maxComponents)
+	: mEntityManager(entityManager), maxComponents(maxComponents),
+      mComponentCount(0) {
+	mComponentIndicesByEntity.reserve(maxComponents);
+	mEntitiesByComponentIndices.reserve(maxComponents);
+}
+
+//	============================================================================
+void EntitySystem::addComponent(const Entity& entity) {
+	assert(mComponentIndicesByEntity.size() < this->maxComponents);
+
+	int cmpntIndex = static_cast<int>(mComponentIndicesByEntity.size());
+
+	mComponentIndicesByEntity.emplace(entity, cmpntIndex);
+	mEntitiesByComponentIndices.emplace(cmpntIndex, entity);
+
+	++mComponentCount;
+
+	this->createComponent(entity);
+}
+
+//	============================================================================
+void EntitySystem::garbageCollect() {
+	if (mComponentIndicesByEntity.size() > 0) {
+		unsigned aliveInRow = 0;
+
+		while (mComponentIndicesByEntity.size() > 0 && aliveInRow < 4) {
+			//	Random index
+			unsigned e = rand() % mComponentIndicesByEntity.size();
+
+			auto pair = std::next(std::begin(mComponentIndicesByEntity), e);
+
+			if (mEntityManager.isAlive(pair->first)) {
+				++aliveInRow;
+				continue;
+			}
+
+			aliveInRow = 0;
+
+			this->removeComponent(pair->first);
+		}
+	}
+}
+
+//	============================================================================
+const Entity& EntitySystem::getEntityByComponentIndex(const int index) const {
+	return mEntitiesByComponentIndices.at(index);
+}
+
+//	============================================================================
+EntityManager& EntitySystem::getEntityManager() { return mEntityManager; }
+
+//	============================================================================
+bool EntitySystem::hasComponent(const Entity& entity) const {
+	return mComponentIndicesByEntity.find(entity) !=
+		   mComponentIndicesByEntity.end();
+}
+
+//	============================================================================
+void EntitySystem::removeComponent(const Entity& entity) {
+	int last       = static_cast<int>(mComponentIndicesByEntity.size() - 1);
+	int cmpntIndex = mComponentIndicesByEntity.at(entity);
+
+	--mComponentCount;
+
+	//	In each component list, swap the specified entity's component
+	//	with the last component in the list
+	this->destroyComponent(cmpntIndex);
+
+	//	Update map to point to swapped component index
+	//	Use at to ensure entry actually exists
+	const Entity lastEntity = mEntitiesByComponentIndices.at(last);
+	mComponentIndicesByEntity[lastEntity] = cmpntIndex;
+	//	Erase after updating swapped component index in case the
+	//	component being removed is the only one in the system.
+	//	unordered_map.erase() doesn't fail if the specified key doesn't
+	//	exist, so there's no need to check the size of the map before
+	//	attempting to erase.
+	mComponentIndicesByEntity.erase(entity);
+
+	//	Have to check if the component being removed is the last one
+	//	or else the map emplace call would add an entry for the removed
+	//	component.
+	mEntitiesByComponentIndices.erase(cmpntIndex);
+	if (entity != lastEntity) {
+		//	Erase the entry for the last entity in map
+		mEntitiesByComponentIndices.erase(last);
+		//	Add new entry for the former last entity and swapped component index
+		mEntitiesByComponentIndices.emplace(cmpntIndex, lastEntity);
+	}
+}
+}

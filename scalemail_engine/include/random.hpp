d85d927632a9f9918e5fbee335db86d935fc7877
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-02-12 10:17:27 -0500

    Added Random class.

---
+++ scalemail_engine/include/random.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <random>
+#include <vector>
+
+namespace ScaleMail
+{
+class Random
+{
+	std::mt19937 mEngine;
+
+public:
+	Random() {
+		std::random_device rd;
+		std::seed_seq seed{rd(), rd(), rd(), rd(), rd(), rd(), rd(), rd()};
+		mEngine = std::mt19937(seed);
+
+	}
+
+	template <typename T>
+	T getRandomElement(const std::vector<T>& v) {
+		if (v.size() == 0) {
+			return nullptr;
+		}
+
+		std::uniform_int_distribution<int> dist(0, v.size() - 1);
+		int index = dist(mEngine);
+		return v[index];
+	}
+};
+}

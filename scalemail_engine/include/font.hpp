340e3c1ab2da8c7eeaef2e63807baef7cb8af424
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-02-13 16:06:12 -0500

    Fixed bad color parameter type in drawText function declaration.

--- scalemail_engine/include/font.hpp
+++ scalemail_engine/include/font.hpp
@@ -10,7 +10,7 @@ struct GameWindow;
 void drawCenterText(const glm::vec2 position, const std::string& text,
 					const glm::vec4 color, const float size);
 void drawText(const glm::vec2 position, const std::string& text,
-			  const glm::vec3 color, const float size);
+			  const glm::vec4 color, const float size);
 void initializeFont(AssetManager& assetManager);
 void renderText(const GameWindow& gameWindow);
 }

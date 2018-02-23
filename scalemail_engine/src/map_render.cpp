2b2730e96f687fb055a7f2ae4950899046fdd3db
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-02-23 13:08:22 -0500

    Changed blend modes for each map mesh in renderMap function. Added vertex count checks before drawing static and alpha meshes in renderMap function.

--- scalemail_engine/src/map_render.cpp
+++ scalemail_engine/src/map_render.cpp
@@ -31,34 +31,41 @@ void initializeMapMesh(AssetManager& assetManager) {
 //	============================================================================
 void renderMap(const Map& map, const Camera& camera,
 			   double totalElapsedSeconds) {
-	//  Draw map
-	const Mesh& mesh = map.mapMesh.staticMesh;
-
 	const glm::mat4 mvp = camera.getProjection() * camera.getView();
 
 	glEnable(GL_DEPTH_TEST);
-	blendAlpha();
+
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	glUseProgram(tileShader.id);
 	glUniform1f(tileShader.timeLocation, 0);
 	glUniformMatrix4fv(tileShader.mvpLocation, 1, GL_FALSE, &mvp[0][0]);
 	glBindTexture(GL_TEXTURE_2D, worldTexture.id);
-	glBindVertexArray(mesh.vao);
-	glDrawArrays(GL_TRIANGLES, 0, mesh.vertexCount);
+
+	const Mesh& mesh = map.mapMesh.staticMesh;
+	if (mesh.vertexCount > 0) {
+		blendNone();
+		glBindVertexArray(mesh.vao);
+		glDrawArrays(GL_TRIANGLES, 0, mesh.vertexCount);
+	}
 
 	const Mesh& alphaMesh = map.mapMesh.alphaMesh;
-	glBindVertexArray(alphaMesh.vao);
-	glDrawArrays(GL_TRIANGLES, 0, alphaMesh.vertexCount);
+	if (alphaMesh.vertexCount > 0) {
+		blendAlpha();
+		glBindVertexArray(alphaMesh.vao);
+		glDrawArrays(GL_TRIANGLES, 0, alphaMesh.vertexCount);
+	}
 
 	glEnable(GL_DEPTH_TEST);
 	const Mesh& animMesh = map.mapMesh.animatedMeshes[tileFrame];
 	if (animMesh.vertexCount > 0) {
+		blendAlpha();
 		glBindVertexArray(animMesh.vao);
 		glDrawArrays(GL_TRIANGLES, 0, animMesh.vertexCount);
 	}
 
 	const Mesh& scrollMesh = map.mapMesh.scrollMeshes[tileFrame];
 	if (scrollMesh.vertexCount > 0) {
+		blendNone();
 		glUniform1f(tileShader.timeLocation, totalElapsedSeconds * 0.5f);
 		glBindTexture(GL_TEXTURE_2D, horzScrollTexture.id);
 		glBindVertexArray(scrollMesh.vao);

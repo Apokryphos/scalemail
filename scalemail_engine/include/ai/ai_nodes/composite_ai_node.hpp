dd8eb96a3c7d6da1145731efe6d0e1e8b6de3e0b
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-03-08 13:56:30 -0500

    Moved CompositeAiNode class code from header to new source file.

--- scalemail_engine/include/ai/ai_nodes/composite_ai_node.hpp
+++ scalemail_engine/include/ai/ai_nodes/composite_ai_node.hpp
@@ -11,28 +11,10 @@ class CompositeAiNode : public AiNode
 	std::vector<std::shared_ptr<AiNode>> mChildNodes;
 
 public:
-	CompositeAiNode(Entity& entity, AiTree* parentTree = nullptr)
-	: AiNode(entity, parentTree) {
-	}
-
-	void addChildNode(std::shared_ptr<AiNode> node) {
-		if (node != nullptr) {
-			mChildNodes.push_back(node);
-		}
-	}
-
-	void addChildNodes(std::vector<std::shared_ptr<AiNode>> nodes) {
-		for (auto node : nodes) {
-			this->addChildNode(node);
-		}
-	}
-
-	bool hasChildNodes() const {
-		return mChildNodes.size() > 0;
-	}
-
-	std::vector<std::shared_ptr<AiNode>> getChildNodes() {
-		return mChildNodes;
-	}
+	CompositeAiNode(Entity& entity, AiTree* parentTree = nullptr);
+	void addChildNode(std::shared_ptr<AiNode> node);
+	void addChildNodes(std::vector<std::shared_ptr<AiNode>> nodes);
+	bool hasChildNodes() const;
+	std::vector<std::shared_ptr<AiNode>> getChildNodes();
 };
 }
---
+++ scalemail_engine/src/ai/ai_nodes/composite_ai_node.cpp
@@ -0,0 +1,33 @@
+#include "ai/ai_nodes/composite_ai_node.hpp"
+
+namespace ScaleMail
+{
+//	============================================================================
+CompositeAiNode::CompositeAiNode(Entity& entity, AiTree* parentTree)
+: AiNode(entity, parentTree) {
+}
+
+//	============================================================================
+void CompositeAiNode::addChildNode(std::shared_ptr<AiNode> node) {
+	if (node != nullptr) {
+		mChildNodes.push_back(node);
+	}
+}
+
+//	============================================================================
+void CompositeAiNode::addChildNodes(std::vector<std::shared_ptr<AiNode>> nodes) {
+	for (auto node : nodes) {
+		this->addChildNode(node);
+	}
+}
+
+//	============================================================================
+bool CompositeAiNode::hasChildNodes() const {
+	return mChildNodes.size() > 0;
+}
+
+//	============================================================================
+std::vector<std::shared_ptr<AiNode>> CompositeAiNode::getChildNodes() {
+	return mChildNodes;
+}
+}

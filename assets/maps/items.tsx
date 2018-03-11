59e7d2bca011705b39dd6894e3522db3bca202d1
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-03-11 09:16:49 -0400

    Changed image source in items.tsx tileset from absolute to relative path.

--- assets/maps/items.tsx
+++ assets/maps/items.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="items" tilewidth="16" tileheight="16" tilecount="96" columns="16">
- <image source="../../../scalemail/assets/textures/items.png" width="256" height="96"/>
+ <image source="../textures/items.png" width="256" height="96"/>
  <tile id="17">
   <properties>
    <property name="Prefab" value="health_potion"/>

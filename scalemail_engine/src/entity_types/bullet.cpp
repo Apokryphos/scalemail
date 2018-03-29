9f8b3e5e473a3d739360c78ac1bb70dc7d893d22
Tony Slavotinek <apokryphos@users.noreply.github.com>
2018-03-29 17:27:51 -0400

    Added bullet particles. Separated bullet particle emitter code from createBullet function.

--- scalemail_engine/src/entity_types/bullet.cpp
+++ scalemail_engine/src/entity_types/bullet.cpp
@@ -14,6 +14,37 @@
 namespace ScaleMail
 {
 //  ============================================================================
+static void addBulletParticleEmitter(World& world, const Entity& entity,
+									 const BulletData& bulletData,
+									 const LightData& lightData) {
+	// Only add particles if alpha is visible
+	ParticleSystem& particleSystem = world.getParticleSystem();
+	particleSystem.addComponent(entity);
+	ParticleComponent particleCmpnt = particleSystem.getComponent(entity);
+
+	ParticleComponentData emitter = {};
+	emitter.life = 0.6f;
+	emitter.decay = 1.0f;
+	emitter.duration = 1.0f;
+	emitter.emitCount = 1;
+	emitter.delay = 0.2f;
+	emitter.interval = 0.01f;
+	emitter.minSize = 0.5f;
+	emitter.maxSize = 2.0f;
+	emitter.minSpeed = bulletData.speed * 0.25f;
+	emitter.maxSpeed = bulletData.speed * 0.33f;
+	emitter.spread = 0.349066f;
+	emitter.direction = glm::vec3(-1, 1, 0.0f);
+	emitter.height = 4.0f;
+	emitter.width = 4.0f;
+	emitter.direction = glm::vec3(-bulletData.direction, 0.0f);
+	emitter.color = lightData.color;
+	emitter.radius = 2.0f;
+
+	particleSystem.setData(particleCmpnt, emitter);
+}
+
+//  ============================================================================
 Entity createBullet(World& world, const Entity& sourceEntity,
 					const glm::vec2& position, const BulletData& bulletData,
 					const LightData& lightData) {
@@ -70,30 +101,7 @@ Entity createBullet(World& world, const Entity& sourceEntity,
 		lightSystem.setPulseSize(lightCmpnt, lightData.pulseSize);
 
 		//	Only add particles if alpha is visible
-		// ParticleSystem& particleSystem = world.getParticleSystem();
-		// particleSystem.addComponent(entity);
-		// ParticleComponent particleCmpnt = particleSystem.getComponent(entity);
-
-		// ParticleComponentData emitter = {};
-		// emitter.life = 0.6f;
-		// emitter.decay = 1.0f;
-		// emitter.duration = 1.0f;
-		// emitter.emitCount = 1;
-		// emitter.delay = 0.2f;
-		// emitter.interval = 0.01f;
-		// emitter.minSize = 0.5f;
-		// emitter.maxSize = 2.0f;
-		// emitter.minSpeed = bulletData.speed * 0.25f;
-		// emitter.maxSpeed = bulletData.speed * 0.33f;
-		// emitter.spread = 0.349066f;
-		// emitter.direction = glm::vec3(-1, 1, 0.0f);
-		// emitter.height = 4.0f;
-		// emitter.width = 4.0f;
-		// emitter.direction = glm::vec3(-bulletData.direction, 0.0f);
-		// emitter.color = lightData.color;
-		// emitter.radius = 2.0f;
-
-		// particleSystem.setData(particleCmpnt, emitter);
+		addBulletParticleEmitter(world, entity, bulletData, lightData);
 	}
 
 	return entity;
